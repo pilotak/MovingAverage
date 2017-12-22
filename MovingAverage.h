0feda2b2f385ebc9d7554c7004276373ca204be3
Pavel Slama <p.slama@atlas.cz>
2017-12-22 17:35:36 +0100

    init

---
+++ MovingAverage.h
@@ -0,0 +1,112 @@
+#ifndef MOVINGAVERAGE_H
+#define MOVINGAVERAGE_H
+
+#include "mbed.h"
+
+template <class T, uint16_t N>
+class MovingAverage {
+ public:
+  MovingAverage();
+  virtual ~MovingAverage(void);
+
+  T add(T value);
+  T get();
+  int32_t get_sum();
+  void fill(T value);
+
+ private:
+  bool _first;
+  uint8_t _next;
+  uint8_t _shift;
+  int32_t _sum;
+
+  T _buffer[N];
+  T _result;
+};
+
+template <class T, uint16_t N>
+MovingAverage<T, N>::MovingAverage():
+  _first(true),
+  _next(0),
+  _sum(0) {
+
+  _result = 0;
+  _shift = 0;
+
+  while (N >> _shift != 1) {
+    _shift++;
+  }
+}
+
+template <class T, uint16_t N>
+MovingAverage<T, N>::~MovingAverage(void) {
+}
+
+template <class T, uint16_t N>
+T MovingAverage<T, N>::get() {
+  return _result;
+}
+template <class T, uint16_t N>
+int32_t MovingAverage<T, N>::get_sum() {
+  return _sum;
+}
+
+template <class T, uint16_t N>
+T MovingAverage<T, N>::add(T value) {
+
+  // fill buffer when using first
+  if (_first) {
+    _first = false;
+
+    fill(value);
+
+  } else {
+    _sum = _sum - (int32_t)_buffer[_next] + (int32_t)value;
+    _buffer[_next] = value;
+    _next = (_next + 1) & (N - 1);
+  }
+
+  _result = (_sum + (N >> 1)) >> _shift; // equals to (_sum + (N / 2)) / N;
+
+  return _result;
+}
+
+template <class T, uint16_t N>
+void MovingAverage<T, N>::fill(T value) {
+  for (uint16_t i = 0; i < N; i++) {
+    _buffer[i] = value;
+  }
+
+  _sum = value * (T)N;
+  _next = 0;
+}
+
+/*template <class T, uint16_t N>
+T MovingAverage<T, N>::get_median(bool hysteresis) {
+  T buffer_sorted[N];
+  T result;
+
+  for (int i = 0; i < N; i++) {
+    buffer_sorted[i] = _buffer[i];
+  }
+
+  for (int i = N - 1; i > 0; --i) {
+    for (int j = 0; j < i; ++j) {
+      if (buffer_sorted[j] > buffer_sorted[j + 1]) {
+        T dTemp = buffer_sorted[j];
+        buffer_sorted[j] = buffer_sorted[j + 1];
+        buffer_sorted[j + 1] = dTemp;
+      }
+    }
+  }
+
+  result = buffer_sorted[(N >> 1)];
+
+  if (hysteresis) {
+    result = ((result + (_hysteresis / 2)) / _hysteresis) * _hysteresis;
+  }
+
+  return result;
+}*/
+
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# MovingAverage
+# Moving average filter
+Moving average buffer filter
+
+**_Buffer_** type can be:
+ - `uint8_t` with maximum buffer length of **16843009**
+ - `uint16_t` with maximum buffer length of **65537**
+ - `uint32_t` but added number can be as long as 31 bits which eaquals to maximum buffer length of **2**, 30bits number = max buffer length of **4**, etc.
+
+**_Buffer length_**:
+- "binary" length only: 2, 4, 8, 16, 32, 64, 128, 254, 512, 1024, 2048, 4096, 8192, 16384
+
+# Example
+```cpp
+#include "mbed.h"
+#include "MovingAverage.h"
+
+ // Buffer (and added samples) will by initialised as uint8_t, total 16 samples
+MovingAverage <uint8_t, 16> filter;
+
+int main() {
+    printf("result: %u\n", filter.add(255)); // insert new number and get result
+    printf("result: %u\n", filter.add(6)); // insert new number and get result
+    printf("result: %u\n", filter.add(9)); // insert new number and get result
+    printf("result: %u\n", filter.get()); // get last result, without adding a newone
+
+    return 0;
+}
+```
+
+## Output
+> result: 255 = 255
+> 
+> result: 239 = 235
+> 
+> result: 224 = 220
